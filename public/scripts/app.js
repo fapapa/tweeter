8dd507eb134e9280a4068814ac416c94f7bade7b
Fabio Papa <fabtheman@gmail.com>
2019-10-09 09:21:40 -0700

    Show the new tweet form when the back-to-top link is clicked

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -40,18 +40,20 @@ const resetValidation = () => {
   $("section.new-tweet .validation-error").hide('fast');
 };
 
+const showNewTweetForm = (event) => {
+  const $newTweetSection = $("section.new-tweet");
+  $newTweetSection.toggle('fast');
+  $newTweetSection.find("textarea").focus();
+  event.preventDefault();
+};
+
 $(document).ready(() => {
   const loadTweets = () => {
     $.ajax("/tweets").then(data => renderTweets(data));
   };
   loadTweets();
 
-  $("#new-tweet-link").click((event) => {
-    const $newTweetSection = $("section.new-tweet");
-    $newTweetSection.toggle('fast');
-    $newTweetSection.find("textarea").focus();
-    event.preventDefault();
-  });
+  $("#new-tweet-link").click(showNewTweetForm);
 
   $("form").on('submit', function(event) {
     const $form = $(this);
@@ -79,6 +81,7 @@ $(document).ready(() => {
 
   $("#back-to-top").click(() => {
     $("html,body").animate({ scrollTop: 0 }, 'fast');
+    showNewTweetForm();
     return false;
   });
 
